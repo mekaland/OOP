6e8d7bc3e7fa2cc18c8a0fbcb11ad0d311c07583
Kadir Dagli <mehmetkadirdagli27@gmail.com>
2024-10-27 20:16:54 +0300

    Add project files.

---
+++ OOP/Musteri.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+     internal class Musteri
+    {
+        public string arac;
+        public string model;
+        public string YakıtTuru;
+        public int modelYıl;
+        public int km;
+        public int vitesTip;
+        public decimal AlisFiyat;
+        public decimal SatisFiyat;
+        public decimal MaxIndirimTutar;
+        public decimal Fiyat;
+
+
+        public Musteri()
+        {
+
+        }
+
+        public Musteri(string _arac,string _model)
+        {
+            arac = _arac;
+            model = _model;
+        }
+
+        public Musteri(string _arac, string _model, int _modelyil)
+        { 
+            arac = _arac;
+            model = _model;
+            modelYıl = _modelyil;
+        }
+
+        public Musteri(string _arac, string _model, int _modelyil, int _km)
+        {
+            arac = _arac;
+            model = _model;
+            modelYıl = _modelyil;
+            km = _km;
+        }
+
+        public void BilgileriGoruntule()
+        {
+        Console.WriteLine("Araç bilgileriniz Aşağıdadır");
+        Console.WriteLine("Araç adı: {0}, Araç modeli: {1}, model yılı: {2}, km: {3}", arac, model, modelYıl,km);
+
+        }
+        public void FiyatAta(decimal _fiyat)
+        {
+            decimal fiyatHesap = SatisFiyat - MaxIndirimTutar;
+            if(_fiyat < fiyatHesap)
+            {
+                Console.WriteLine("Geçersiz fiyat girişi yaptınız");
+
+            }else
+            {
+                 this.Fiyat =  _fiyat;
+                Console.WriteLine("Fiyat Güncellendi");
+            }
+        }
+
+    }
+    
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Musteri musteri = new Musteri("Opel", "Corsa",2010,56000);
+            musteri.AlisFiyat = 28000;
+            musteri.SatisFiyat = 32000;
+            musteri.MaxIndirimTutar = 1500;
+            musteri.FiyatAta(40000);
+            musteri.BilgileriGoruntule();
+            
+        
+            
+
+            Console.ReadKey();
+
+        }
+    }
+
+    
+}
